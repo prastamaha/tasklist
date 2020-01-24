8dc2d87a50a54d1679133ecc6d672f7a170cc064
prastamaha <prastamaha@gmail.com>
2020-01-24 18:01:34 +0800

    Initial commit

--- todoapp/settings.py
+++ todoapp/settings.py
@@ -49,8 +49,8 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-# SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-# SECURE_SSL_REDIRECT = True
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_SSL_REDIRECT = True
 
 ROOT_URLCONF = 'todoapp.urls'
 
