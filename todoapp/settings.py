b7f0de8ffeb4bb42b8c9342327e94712a00f5381
prastamaha <prastamaha@gmail.com>
2020-02-09 17:37:55 +0800

    update

--- todoapp/settings.py
+++ todoapp/settings.py
@@ -127,8 +127,10 @@ USE_TZ = True
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"), 
+    os.path.join(BASE_DIR, 'media'),
     ]
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
